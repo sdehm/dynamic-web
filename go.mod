5259f514c9c217877797aac96719c4d496bb192e
Shaffer Dehmlow <sdehm@github.com>
2022-11-10 14:12:38 -0700

    Try x/net/websocket

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/sdehm/go-morph
 
 go 1.18
+
+require golang.org/x/net v0.2.0
--- server/server.go
+++ server/server.go
@@ -2,7 +2,10 @@ package server
 
 import (
 	"embed"
+	"fmt"
 	"net/http"
+
+	"golang.org/x/net/websocket"
 
 	"github.com/sdehm/go-morph/templates"
 )
@@ -21,6 +24,7 @@ func Start(templates *templates.Templates) *Server {
 	}
 	http.HandleFunc("/", indexHandler(server))
 	http.Handle("/public/", http.FileServer(http.FS(public)))
+	http.Handle("/echo", websocket.Handler(EchoServer))
 	http.ListenAndServe(":8080", nil)
 	return server
 }
@@ -29,4 +33,17 @@ func indexHandler(s *Server) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		s.templates.Pages["index"].Execute(w, nil)
 	}
+}
+
+// Echo the data received on the WebSocket.
+func EchoServer(ws *websocket.Conn) {
+	fmt.Println("done")
+
+	for {
+		var message string
+		websocket.Message.Receive(ws, &message)
+		fmt.Println("Received back from client: " + message)
+	}
+	
+	// io.Copy(ws, ws)
 }
