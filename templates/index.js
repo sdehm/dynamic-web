746283b959b4651d091ce6c6be475a670ca25e8d
Shaffer Dehmlow <sdehm@github.com>
2022-11-12 15:54:31 -0700

    Format js

--- templates/index.js
+++ templates/index.js
@@ -1,15 +1,15 @@
-import morphdom from 'morphdom';
+import morphdom from "morphdom";
 
 // connect to websocket
-const socket = new WebSocket('ws://localhost:8080/ws');
+const socket = new WebSocket("ws://localhost:8080/ws");
 
 socket.onopen = () => {
-  console.log('Connected');
-}
+  console.log("Connected");
+};
 
 socket.onmessage = (event) => {
   const data = JSON.parse(event.data);
   const id = data.id;
-  const html = data.html
+  const html = data.html;
   morphdom(document.getElementById(id), html);
-}
+};
